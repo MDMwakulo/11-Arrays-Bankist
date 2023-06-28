2488f7375c89c4cffb2d22a4910a156ca4c71e6f
MDMwakulo <mdmwakulo@gmail.com>
2023-06-28 19:04:51 +0300

    The map Method

--- script.js
+++ script.js
@@ -84,7 +84,7 @@ displayMovements(account1.movements);
 /////////////////////////////////////////////////
 // LECTURES
 
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
@@ -181,6 +181,7 @@ currenciesUnique.forEach(function (value, _, map) {
 });
 */
 
+/*
 // Coding Challenge #1
 const checkDogs = function (dogsJulia, dogsKate) {
   const updatedDogsJulia = dogsJulia.slice();
@@ -202,3 +203,22 @@ const julia1 = [3, 5, 2, 12, 7],
 checkDogs(julia1, kate1);
 console.log(`======= TEST CASE 2 =======`);
 checkDogs(julia2, kate2);
+*/
+
+// map method
+const eurToUsd = 1.1;
+
+const movementsUSD = movements.map(mov => mov * eurToUsd);
+
+console.log(movements); // [200, 450, -400, 3000, -650, -130, 70, 1300]
+console.log(movementsUSD); // [220.00000000000003, 495.00000000000006, -440.00000000000006, 3300.0000000000005, -715.0000000000001, -143, 77, 1430.0000000000002]
+
+const movementsUSDfor = [];
+for (const mov of movements) movementsUSDfor.push(mov * eurToUsd);
+console.log(movementsUSDfor);
+
+const movementsDescriptions = movements.map(
+  (mov, i) =>
+    `Movement ${i + 1}: You ${mov > 0 ? `deposited` : `widthrew`} ${mov}`
+);
+console.log(movementsDescriptions);
