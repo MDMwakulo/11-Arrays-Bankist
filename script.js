58bcc68fffdb20cc05f3bcb7e4b4bbd1431b2148
MDMwakulo <mdmwakulo@gmail.com>
2023-07-07 16:47:44 +0300

    some and every

--- script.js
+++ script.js
@@ -183,6 +183,24 @@ btnTransfer.addEventListener('click', function (e) {
   }
 });
 
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    // Add movement
+    currentAccount.movements.push(amount);
+
+    // Update the UI
+    updateUI(currentAccount);
+  }
+
+  inputLoanAmount.value = ``;
+  inputLoanAmount.blur();
+  y;
+});
+
 btnClose.addEventListener('click', function (e) {
   e.preventDefault();
 
@@ -466,3 +484,30 @@ for (const acc of accounts) {
   }
 }
 */
+
+console.log(movements);
+
+// EQUALITY
+console.log(movements.includes(-130));
+
+// CONDITION
+const anyDeposits = movements.some(mov => mov > 1500);
+console.log(anyDeposits);
+
+// SOME: CONDITION
+console.log(movements.some(mov => mov === -130));
+
+const anyDeposit = movements.some(mov => mov > 0);
+console.log(anyDeposit);
+
+// EVERY
+console.log(movements.every(mov => mov < 0));
+console.log(account4.movements.push(-1));
+console.log(account4.movements.every(mov => mov < 0));
+
+// Separate callback
+console.log(movements); // [200, 450, -400, 3000, -650, -130, 70, 1300]
+const deposit = mov => mov > 0;
+console.log(movements.some(deposit)); // true
+console.log(movements.every(deposit)); // false
+console.log(movements.filter(deposit)); // [200, 450, 3000, 70, 1300]
